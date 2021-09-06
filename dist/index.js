aa54cc2ae031f308822aa0e183adcacebde1a920
pr-bot-test <88473393+pr-bot-test@users.noreply.github.com>
2021-09-06 17:28:37 +0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -6322,8 +6322,8 @@ try {
   const github_token = core.getInput("GITHUB_TOKEN");
   const Labels = core.getInput("labels").split(",");
   const labelsObject = github.context.payload.issue.labels;
-  const octokit = github.getOctokit(github_token);
-
+  const octokit = github.getOctokit(github_token); 
+  log(github.context.payload)
   let issueLabels = [];
 
   labelsObject.forEach((item, index) => issueLabels.push(item.name));
@@ -6349,7 +6349,7 @@ try {
       issue_number: github.context.issue.number,
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
-      body: message,
+      body: github.context.payload.issue,
     });
   }
 } catch (error) {
